65f097c2b43298102764ca27966106c856558e9f
archerC <brightcxl@gmail.com>
2019-08-02 18:46:25 +0800

    添加启动脚本

--- scripts/autorun.cmd
+++ scripts/autorun.cmd
@@ -15,12 +15,13 @@ if %ERRORLEVEL% neq 0 (
 
 REM 我的默认配置
 set SYSTEM_PATH=%PATH%
+set CONDA_BIN=D:\Programs\Anaconda3\condabin
 set GIT_BIN=D:\Programs\Git\cmd
 set PANDOC_BIN=D:\Programs\Anaconda3\Scripts
 set VIM_BIN=D:\Programs\Vim\vim81
 set GTAGS_BIN=D:\Programs\global\bin
 set TEX_BIN=D:\Programs\texlive\2018\bin\win32
-set PATH=%SYSTEM_PATH%;%VIM_BIN%;%GTAGS_BIN%;%TEX_BIN%;%GIT_BIN%
+set PATH=%SYSTEM_PATH%;%VIM_BIN%;%GTAGS_BIN%;%TEX_BIN%;%GIT_BIN%;%CONDA_BIN%
 
 REM 设置命令提示符
 prompt $P$G$_$D$B$T$G$S$$$S
